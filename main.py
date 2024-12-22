54696ce7b1f9ab72b92a07ed25d982e580848dec
heaphopper <tzvienir@gmail.com>
2024-12-22 19:49:10 +0200

    more streamlit features

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+# Title for the app
+st.title("Streamlit Radio Button Example")
+
+
 st.markdown("""
 # Title
 ## Subtitle
@@ -10,3 +14,21 @@ st.markdown("""
 
 > Amazong quote
 """)
+
+
+# Radio button options
+options = ["Option 1", "Option 2", "Option 3"]
+
+# Adding the radio button
+choice = st.radio("Choose an option:", options)
+
+# Displaying the result based on user choice
+if choice == "Option 1":
+    st.write("You selected Option 1! 🎉")
+elif choice == "Option 2":
+    st.write("You selected Option 2! 🚀")
+elif choice == "Option 3":
+    st.write("You selected Option 3! 🌟")
+
+# Add any additional content or functionality here
+st.write("Feel free to make another selection above!")
